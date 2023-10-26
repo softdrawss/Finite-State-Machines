c3a49c61f302a42bcc2ce17155c9cfec90847ec5
Paules23 <pauvz123@gmail.com>
2023-10-26 12:49:40 +0200

    its perfectly done except for the wander that for some reason its not working entirely propperly

--- Robbery/Assets/Scripts/Moves.cs
+++ Robbery/Assets/Scripts/Moves.cs
@@ -67,11 +67,13 @@ public class Moves : MonoBehaviour
         float wanderDistance = 10;
         float wanderJitter = 1;
 
+
         wanderTarget += new Vector3(Random.Range(-1.0f, 1.0f) * wanderJitter,
                                         0,
                                         Random.Range(-1.0f, 1.0f) * wanderJitter);
         wanderTarget.Normalize();
         wanderTarget *= wanderRadius;
+
 
         Vector3 targetLocal = wanderTarget + new Vector3(0, 0, wanderDistance);
         Vector3 targetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
